d68a6d115c1dc5e740eba7f9d092381bfd2b1b28
Nina Martin <nina.mm.martin@gmail.com>
2021-05-20 21:13:43 +0100

    Remove fluff

--- app/main.cpp
+++ app/main.cpp
@@ -1,15 +1,6 @@
-// Executables must have the following defined if the library contains
-// doctest definitions. For builds with this disabled, e.g. code shipped to
-// users, this can be left out.
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest.h"
-#endif
-
 #include <iostream>
 #include <stdlib.h>
 
-#include "exampleConfig.h"
 #include "example.h"
 
 /*
@@ -17,16 +8,7 @@
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  std::cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << "."
-            << PROJECT_VERSION_PATCH
-            << "."
-            << PROJECT_VERSION_TWEAK
-            << std::endl;
-  std::system("cat ../LICENSE");
+  std::cout << "Hello World" << std::endl;
 
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
--- src/example.cpp
+++ src/example.cpp
@@ -10,12 +10,3 @@ bool Dummy::doSomething() {
     auto [zero, one] = digits;
     return zero + one;
 }
-
-
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-#include "doctest.h"
-TEST_CASE("we can have tests written here, to test impl. details")
-{
-    CHECK(true);
-}
-#endif
